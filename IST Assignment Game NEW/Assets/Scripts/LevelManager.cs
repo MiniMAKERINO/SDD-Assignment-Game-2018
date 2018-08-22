990efb5bf2190139fe842900edf975c7bde2fe7d
MiniMAKERINO <jd_mak@yahoo.com.au>
2018-08-22 16:08:20 +1000

    Main Menu Done

---
+++ IST Assignment Game NEW/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene("Scene 1");
+
+    }
+
+    public void QuitGame() {
+        Debug.Log("QUIT works just fine!"); 
+        Application.Quit();
+    }
+
+
+}
